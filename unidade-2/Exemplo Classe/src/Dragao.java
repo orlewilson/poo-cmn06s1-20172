bc9259a1e37e4ba7acf8965e8d4d087b5ba131ef
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2017-09-21 19:53:03 -0400

    atualizacao da classe para garantir o encapsulamento dos atributos

--- unidade-2/Exemplo Classe/src/Dragao.java
+++ unidade-2/Exemplo Classe/src/Dragao.java
@@ -11,10 +11,57 @@
 public class Dragao {
 
 	// atributos (sem encapsulamento)
-	double tamanho;
-	double peso;
-	char sexo;
-	String cor;
-	int idade;
-	
+	// double tamanho;
+	// double peso;
+	// char sexo;
+	// String cor;
+	// int idade;
+
+	// atributos (com encapsulamento)
+	private double tamanho;
+	private double peso;
+	private char sexo;
+	private String cor;
+	private int idade;
+
+	// Métodos para acessar os atributos (encapsulamento)
+	public double getTamanho() {
+		return this.tamanho;
+	}
+
+	public void setTamanho(double tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public double getPeso() {
+		return this.peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public char getSexo() {
+		return this.sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCor() {
+		return this.cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getIdade() {
+		return this.idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 }
--- unidade-2/Exemplo Classe/src/Principal.java
+++ unidade-2/Exemplo Classe/src/Principal.java
@@ -14,24 +14,35 @@ public class Principal {
 
 		// criando objeto do tipo dragao
 		Dragao smaug = new Dragao();
-		
+
 		// atribuindo valores aos atributos (sem encapsulamento)
-		smaug.cor = "preto";
-		smaug.idade = 345;
-		smaug.peso = 360.6;
-		smaug.sexo = 'M';
-		smaug.tamanho = 60;
-		
+		// smaug.cor = "preto";
+		// smaug.idade = 345;
+		// smaug.peso = 360.6;
+		// smaug.sexo = 'M';
+		// smaug.tamanho = 60;
+
+		// atribuindo valores aos atributos (com encapsulamento)
+		smaug.setCor("preto");
+		smaug.setIdade(345);
+		smaug.setPeso(360.6);
+		smaug.setSexo('M');
+		smaug.setTamanho(60);
+
 		// mostrando valores dos atributos no
 		// console (sem encapsulamento)
-		System.out.println("Cor: " + smaug.cor);
-		System.out.println("Idade: " + 
-				smaug.idade);
-		System.out.println("Peso: " + smaug.peso);
-		System.out.println("Sexo: " + smaug.sexo);
-		System.out.println("Tamanho: " + 
-				smaug.tamanho);
+//		System.out.println("Cor: " + smaug.cor);
+//		System.out.println("Idade: " + smaug.idade);
+//		System.out.println("Peso: " + smaug.peso);
+//		System.out.println("Sexo: " + smaug.sexo);
+//		System.out.println("Tamanho: " + smaug.tamanho);
 		
-		
+		// mostrando valores dos atributos no
+		// console (com encapsulamento)
+		System.out.println("Cor: " + smaug.getCor());
+		System.out.println("Idade: " + smaug.getIdade());
+		System.out.println("Peso: " + smaug.getPeso());
+		System.out.println("Sexo: " + smaug.getSexo());
+		System.out.println("Tamanho: " + smaug.getTamanho());
 	}
 }
